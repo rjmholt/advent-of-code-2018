b9be85af2e32abc7ed1745a0fd034c1084cf3bbe
Robert Holt <rjmholt_msft@outlook.com>
2018-12-03 20:48:02 -0800

    Day 1_1

--- src/puzzles/day1.rs
+++ src/puzzles/day1.rs
@@ -1,3 +1,30 @@
 pub fn day1_1(input: String) -> String {
-    "Hello".to_string()
+    let mut sum: i32 = 0;
+    for line in input.lines() {
+        if line.len() == 0 {
+            continue
+        }
+        let val = get_number(line);
+        sum += val;
+    }
+    format!("{}", sum)
+}
+
+fn get_number(line: &str) -> i32 {
+    let sign: &str = &line[0..1];
+    let num: &str = &line[1..];
+
+    match sign {
+        "+" => {
+            num.parse().unwrap()
+        }
+
+        "-" => {
+            - num.parse::<i32>().unwrap()
+        }
+
+        _ => {
+            panic!("Bad number format in input file")
+        }
+    }
 }
